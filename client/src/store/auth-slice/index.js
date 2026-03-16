6d9e7d3bb49428bb02ff3f6bad45e8408f4d8ba1
Prashant Goit <prashant.goit@student.sl.on.ca>
2026-03-17 01:11:43 +0530

    Update registration API endpoint to production URL

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/register",
+      "https://ecommerce-60i5.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
